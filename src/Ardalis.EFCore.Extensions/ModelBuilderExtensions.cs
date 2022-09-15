609701b42f20127c02b768027edf671c4440a185
Steve Smith <steve@kentsmiths.com>
2022-09-15 16:33:03 -0400

    Modifying to use net6 and wrap EF Core 6 implementation to fix issue #2

--- src/Ardalis.EFCore.Extensions/ModelBuilderExtensions.cs
+++ src/Ardalis.EFCore.Extensions/ModelBuilderExtensions.cs
@@ -4,52 +4,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 
-namespace Ardalis.EFCore.Extensions
+namespace Ardalis.EFCore.Extensions;
+
+/// <summary>
+/// Original: https://stackoverflow.com/a/47263024/13729
+/// </summary>
+public static class ModelBuilderExtensions
 {
-    /// <summary>
-    /// Original: https://stackoverflow.com/a/47263024/13729
-    /// </summary>
-    public static class ModelBuilderExtensions
-    {
-        /// <summary>
-        /// Applies all configurations defined in this assembly.
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        /// <param name="assembly">Optional. The assembly in which the config classes are located.</param>
-        /// <param name="configNamespace">Optional. If provided, only configurations in this namespace will be applied.</param>
-        public static void ApplyAllConfigurationsFromCurrentAssembly(this ModelBuilder modelBuilder, Assembly assembly = null, string configNamespace = "")
-        {
-            var applyGenericMethods = typeof(ModelBuilder).GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-            var applyGenericApplyConfigurationMethods = applyGenericMethods.Where(m => m.IsGenericMethod && m.Name.Equals("ApplyConfiguration", StringComparison.OrdinalIgnoreCase));
-            var applyGenericMethod = applyGenericApplyConfigurationMethods.Where(m => m.GetParameters().FirstOrDefault().ParameterType.Name == "IEntityTypeConfiguration`1").FirstOrDefault();
-
-            var callingAssembly = new StackFrame(1).GetMethod().DeclaringType.Assembly;
-            var assemblyToUse = assembly ?? callingAssembly;
-            var applicableTypes = assemblyToUse
-                .GetTypes()
-                .Where(c => c.IsClass && !c.IsAbstract && !c.ContainsGenericParameters);
-
-            if (!string.IsNullOrEmpty(configNamespace))
-            {
-                applicableTypes = applicableTypes.Where(c => c.Namespace == configNamespace);
-            }
-
-            foreach (var type in applicableTypes)
-            {
-                foreach (var iface in type.GetInterfaces())
-                {
-                    // if type implements interface IEntityTypeConfiguration<SomeEntity>
-                    if (iface.IsConstructedGenericType && iface.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>))
-                    {
-                        // make concrete ApplyConfiguration<SomeEntity> method
-                        var applyConcreteMethod = applyGenericMethod.MakeGenericMethod(iface.GenericTypeArguments[0]);
-                        // and invoke that with fresh instance of your configuration type
-                        applyConcreteMethod.Invoke(modelBuilder, new object[] { Activator.CreateInstance(type) });
-                        Console.WriteLine("applied model " + type.Name);
-                        break;
-                    }
-                }
-            }
-        }
-    }
+	/// <summary>
+	/// Applies all configurations defined in this assembly.
+	/// </summary>
+	/// <param name="modelBuilder"></param>
+	/// <param name="assembly">Optional. The assembly in which the config classes are located.</param>
+	/// <param name="configNamespace">Optional. If provided, only configurations in this namespace will be applied.</param>
+	public static void ApplyAllConfigurationsFromCurrentAssembly(this ModelBuilder modelBuilder,
+		Assembly? assembly = null, string configNamespace = "")
+	{
+		if (assembly == null) assembly = Assembly.GetCallingAssembly();
+		modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+	}
 }
