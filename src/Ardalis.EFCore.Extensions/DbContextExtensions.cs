50ca91b9c9b6d89bb0da3cbdbd7245313e6975e3
Hamid Reza Ashkiyan <HamidRezaAshkiyan@gmail.com>
2023-11-24 13:20:54 +0330

    Add tracking extensions.

---
+++ src/Ardalis.EFCore.Extensions/DbContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ardalis.EFCore.Extensions;
+
+public static class DbContextExtensions
+{
+    /// <summary>
+    /// Enable Tracking for DbContext
+    /// </summary>
+    /// <param name="dbContext"></param>
+    public static void EnableTracking(this DbContext dbContext)
+    {
+        dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+    }
+
+    /// <summary>
+    /// Enable Tracking for DbContext
+    /// </summary>
+    /// <param name="dbContext"></param>
+    public static void DisableTracking(this DbContext dbContext)
+    {
+        dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+}
